dc9c1f2a85876c71163e552359f8e9646a6f2d28
chokiproai <Dogaopc13@gmail.com>
2024-01-28 12:54:46 +0700

    Fix

--- frontend/src/components/ChatPromptStore/ChatPromptItem.vue
+++ frontend/src/components/ChatPromptStore/ChatPromptItem.vue
@@ -8,19 +8,19 @@ defineProps<{
   source: IPrompt;
 }>();
 
-const messgae = useMessage();
+const message = useMessage();
 const promptStore = usePromptStore();
 const { promptList, optPromptConfig } = storeToRefs(promptStore);
 
 const delPrompt = (item: IPrompt) => {
   promptList.value = promptList.value.filter((x) => x.act !== item.act && x.prompt !== item.prompt);
-  messgae.success('Đã xóa từ gợi ý');
+  message.success('Deleted suggestion');
 };
 
 const showEditPromptPop = (item: IPrompt) => {
   optPromptConfig.value.isShow = true;
   optPromptConfig.value.type = 'edit';
-  optPromptConfig.value.title = 'Chỉnh sửa từ gợi ý';
+  optPromptConfig.value.title = 'Edit suggestion';
   optPromptConfig.value.tmpPrompt = item;
   optPromptConfig.value.newPrompt = { ...item };
 };
@@ -33,8 +33,8 @@ const showEditPromptPop = (item: IPrompt) => {
         <span class="inline-block max-w-[120px] xl:max-w-[650px] overflow-hidden text-ellipsis">{{ source.act }}</span>
       </NTag>
       <div class="float-right">
-        <NButton secondary type="info" size="small" @click="showEditPromptPop(source)">Chỉnh sửa</NButton>
-        <NButton secondary class="ml-2" type="error" size="small" @click="delPrompt(source)">Xóa</NButton>
+        <NButton secondary type="info" size="small" @click="showEditPromptPop(source)">Edit</NButton>
+        <NButton secondary class="ml-2" type="error" size="small" @click="delPrompt(source)">Delete</NButton>
       </div>
     </template>
     <NEllipsis :tooltip="false" :line-clamp="2">{{ source.prompt }}</NEllipsis>
--- frontend/src/components/ChatPromptStore/ChatPromptStore.vue
+++ frontend/src/components/ChatPromptStore/ChatPromptStore.vue
@@ -6,7 +6,7 @@ import { storeToRefs } from 'pinia';
 import VirtualList from 'vue3-virtual-scroll-list';
 import ChatPromptItem from './ChatPromptItem.vue';
 
-const messgae = useMessage();
+const message = useMessage();
 const promptStore = usePromptStore();
 const { promptDownloadConfig, isShowPromptSotre, promptList, keyword, searchPromptList, optPromptConfig } = storeToRefs(promptStore);
 
@@ -28,39 +28,124 @@ const showAddPromptPop = () => {
 const savePrompt = () => {
   const { type, tmpPrompt, newPrompt } = optPromptConfig.value;
   if (!newPrompt.act) {
-    return messgae.error('Suggestion title cannot be empty');
+    return message.error('Suggestion title should not be empty');
   }
   if (!newPrompt.prompt) {
-    return messgae.error('Suggestion description cannot be empty');
+    return message.error('Suggestion description should not be empty');
   }
   if (type === 'add') {
     promptList.value = [newPrompt, ...promptList.value];
-    messgae.success('Successfully added suggestion');
+    message.success('Successfully added suggestion');
   } else if (type === 'edit') {
     if (newPrompt.act === tmpPrompt?.act && newPrompt.prompt === tmpPrompt?.prompt) {
-      messgae.warning('Suggestion did not change');
+      message.warning('Suggestion did not change');
       optPromptConfig.value.isShow = false;
       return;
     }
     const rawIndex = promptList.value.findIndex((x) => x.act === tmpPrompt?.act && x.prompt === tmpPrompt?.prompt);
     if (rawIndex > -1) {
       promptList.value[rawIndex] = newPrompt;
-      messgae.success('Successfully edited suggestion');
+      message.success('Successfully edited suggestion');
     } else {
-      messgae.error('Error editing suggestion');
+      message.error('Edit suggestion failed');
     }
   }
   optPromptConfig.value.isShow = false;
 };
 
-// ... (rest of the script code)
+const readFile = (file: File): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = function (ev) {
+      resolve(ev.target?.result as string);
+    };
+    reader.onerror = reject;
+    reader.readAsText(file);
+  });
+};
 
-</script>
+const importPrompt = async (options: { file: UploadFileInfo; fileList: Array<UploadFileInfo>; event?: Event }) => {
+  // console.log(options.file);
+  if (options.file.file) {
+    isImporting.value = true;
+    const fileText = await readFile(options.file.file);
+    const promptData = JSON.parse(fileText);
+    const result = promptStore.addPrompt(promptData);
+    if (result.result) {
+      message.info(`Upload file contains ${promptData.length} data`);
+      message.success(`Successfully imported ${result.data?.successCount} valid data`);
+    } else {
+      message.error(result.msg || 'Suggestion format error');
+    }
+    isImporting.value = false;
+  } else {
+    message.error('Upload file error');
+  }
+};
+
+const exportPrompt = () => {
+  if (promptList.value.length === 0) {
+    return message.error('No suggestion data to export');
+  }
+  isExporting.value = true;
+  const jsonDataStr = JSON.stringify(promptList.value);
+  const blob = new Blob([jsonDataStr], { type: 'application/json' });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = 'BingAIPrompts.json';
+  link.click();
+  URL.revokeObjectURL(url);
+  message.success('Successfully exported suggestion store');
+  isExporting.value = false;
+};
+
+const clearPrompt = () => {
+  promptList.value = [];
+  message.success('Cleared suggestion store');
+};
+
+const downloadPrompt = async (config: IPromptDownloadConfig) => {
+  if (!config.url) {
+    return message.error('Please enter download link first');
+  }
+  config.isDownloading = true;
+  let jsonData: Array<IPrompt>;
+  if (config.url.endsWith('.json')) {
+    jsonData = await fetch(config.url).then((res) => res.json());
+  } else if (config.url.endsWith('.csv')) {
+    const csvData = await fetch(config.url).then((res) => res.text());
+    console.log(csvData);
+    jsonData = csvData
+      .split('\n')
+      .filter((x) => x)
+      .map((x) => {
+        const arr = x.split('","');
+        return {
+          act: arr[0].slice(1),
+          prompt: arr[1]?.slice(1),
+        };
+      });
+    jsonData.shift();
+  } else {
+  config.isDownloading = false;
+  return message.error('Temporarily do not support download suggestions with this extension');
+  }
+  config.isDownloading = false;
+  const result = promptStore.addPrompt(jsonData);
+  if (result.result) {
+    message.info(`Download file contains ${jsonData.length} data`);
+    message.success(`Successfully imported ${result.data?.successCount} valid data`);
+  } else {
+    message.error(result.msg || 'Suggestion format error');
+  }
+  };
+  </script>
 
 <template>
   <NModal class="w-11/12 xl:w-[900px]" v-model:show="isShowPromptSotre" preset="card" title="Suggestion Store">
     <div class="flex justify-start flex-wrap gap-2 px-5 pb-2">
-      <NInput class="basis-full xl:basis-0 xl:min-w-[300px]" placeholder="Search suggestion" v-model:value="keyword" :clearable="true"></NInput>
+      <NInput class="basis-full xl:basis-0 xl:min-w-[300px]" placeholder="Search suggestions" v-model:value="keyword" :clearable="true"></NInput>
       <NButton secondary type="info" @click="isShowDownloadPop = true">Download</NButton>
       <NButton secondary type="info" @click="showAddPromptPop">Add</NButton>
       <NUpload class="w-[56px] xl:w-auto" accept=".json" :default-upload="false" :show-file-list="false" @change="importPrompt">
@@ -80,7 +165,7 @@ const savePrompt = () => {
     />
     <NEmpty v-else class="h-[40vh] xl:h-[60vh] flex justify-center items-center" description="No data">
       <template #extra>
-        <NButton secondary type="info" @click="isShowDownloadPop = true">Download suggestion</NButton>
+        <NButton secondary type="info" @click="isShowDownloadPop = true">Download Suggestions</NButton>
       </template>
     </NEmpty>
   </NModal>
@@ -93,7 +178,7 @@ const savePrompt = () => {
       <NButton block secondary type="info" @click="savePrompt">Save</NButton>
     </NSpace>
   </NModal>
-  <NModal class="w-11/12 xl:w-[600px]" v-model:show="isShowDownloadPop" preset="card" title="Download suggestion">
+  <NModal class="w-11/12 xl:w-[600px]" v-model:show="isShowDownloadPop" preset="card" title="Download Suggestions">
     <NList class="overflow-y-auto rounded-lg" hoverable clickable>
       <NListItem v-for="(config, index) in promptDownloadConfig" :key="index">
         <a v-if="config.type === 1" class="no-underline text-blue-500" :href="config.url" target="_blank" rel="noopener noreferrer">{{ config.name }}</a>
