17ee18978fc6984a84a18906e5020cb9b8c2ef50
chokiproai <dogaopc13@gmail.com>
2024-09-03 09:15:10 +0000

    [Upload] WebFile

---
+++ web/js/bing/chat/core.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+(function (n, t) {
+  onload = function () {
+    _G.BPT = new Date();
+    !_w.sb_ppCPL && t && sb_st(function () {
+      t(new Date());
+    }, 0);
+  }
+    ;
+}
+)(_w.onload, _w.si_PP);
+_w.rms.js(
+  { 'A:rms:answers:Shared:BingCore.Bundle': '/rp/nAb2_uMWSHAhScje1ODrwe4cVoA.br.js' },
+  // { 'A:rms:answers:Web:FreeSydneyHelper': '/rp/cfhgV3_HFyB3URSxigzE0QzgsF8.br.js' },
+  // { 'A:rms:answers:Web:SydneyFSCHelper': '/rp/wJbTArpXX7iY8vTObyrSJ0XZMCE.br.js' },
+  { 'A:rms:answers:VisualSystem:ConversationScope': '/rp/ascyKFaOzJnUTZ_D-Mv819BZ0Go.br.js' },
+  { 'A:rms:answers:CodexBundle:cib-bundle': '/rp/V6IKxmcE5rJ2bGlG-v3sIvR-550.br.js' },
+  { 'A:rms:answers:SharedStaticAssets:speech-sdk': '/rp/bll21ZO27j3KPE27uQBxt24c2Fw.br.js' },
+  { 'A:rms:answers:Web:SydneyWelcomeScreenBase':'/rp/KgDPK_mKWjMgWe6o0SKA9dfE00Y.br.js' },
+  { 'A:rms:answers:Web:SydneyWelcomeScreen':'/rp/sNzL8vfHHNmyqYOSQey9fNYd3kI.br.js' },
+  { 'A:rms:answers:Web:SydneyFullScreenConv': '/rp/QGOwWXVSYoKX5FtgJZTIy7dpusc.br.js' },
+  // { 'A:rms:answers:Web:ChatHomeScreenBase': '/rp/SlXkn2s3KjsnNrD80OtYBr8qECA.br.js' },
+  // { 'A:rms:answers:Web:ChatHomeScreen': '/rp/x05EBLDKo9Gp0rV1W9OmmpoFp0o.br.js' },
+);
